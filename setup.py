acae23b140a90b413313863e754e094fea50c7bf
MD. Abu Bakar Siddique <140780598+Abs-Futy7@users.noreply.github.com>
2025-04-11 01:37:55 +0600

    Adding files

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+REQUIREMENT_FILE_NAME = "requirements.txt"
+REMOVE_PACKAGE = "-e ."
+
+
+
+setup(
+    name="google dialogflow chatbot",
+    license="MIT",
+    version="0.0.0",
+    description="chatbot suiii",
+    author="Abs",
+    packages=find_packages(),
+
+)
